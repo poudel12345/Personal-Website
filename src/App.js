eeaaedbe3eb954ac8e9b951a6b4cfede193d8b5c
poudel12345 <prafulpoudel21@gmail.com>
2021-08-24 18:47:08 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
@@ -9,3 +9,5 @@ export default function App() {
     </div>
   );
 }
+
+//this will have the routing to all my components
