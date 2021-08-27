7d67f7d03a842e0236930f1d4605075e7a5819b0
poudel12345 <prafulpoudel21@gmail.com>
2021-08-26 23:47:22 -0400

    1 file modified

--- src/components/template/SideBar.js
+++ src/components/template/SideBar.js
@@ -7,7 +7,8 @@ const { PUBLIC_URL } = process.env; // set automatically
 
 const SideBar = () => (
   <section id="sideaber">
-    //insert an image link with your photo /*
+    //insert an image link with your photo
+     /*
     <Link to="/" className="logo">
       <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
     </Link>
@@ -23,19 +24,49 @@ const SideBar = () => (
       */
     </header>
   </section>
+
+
+<section className="blurb">
+  <h2> About </h2>
+  <p>
+    {' '}
+    Hi I am Praful. I am learning react and am fluent at C++, Java and basic web
+    development
+  </p>
+  //here we will insert some links to the other pages of the website
+  <ul className="actions">
+    <li>
+      /*
+      {!window.location.pathname.includes('/resume') ? (
+        <Link to="/resume" className="button">
+          Learn More
+        </Link>
+      ) : (
+        <Link to="/about" className="button">
+          About Me
+        </Link>
+      )}
+      */
+    </li>
+  </ul>
+</section>;
 );
 
-
-
+export default SideaBar;
 //you can set the top of the side bar here
 /*
-<ul className="actions">
-<li>
-  {!window.location.pathname.includes('/resume') ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/about" className="button">About Me</Link>}
-</li>
-</ul>
-*/
+  <ul className="actions">
+    <li>
+      {!window.location.pathname.includes('/resume') ? (
+        <Link to="/resume" className="button">
+          Learn More
+        </Link>
+      ) : (
+        <Link to="/about" className="button">
+          About Me
+        </Link>
+      )}
+    </li>
+  </ul>
+  */
 
-</section>
-
-export default SideaBar;
