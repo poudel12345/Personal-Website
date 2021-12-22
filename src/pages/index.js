5c1a26e058e8bac386f279ff9db82771037ec101
poudelpatwit <prafulpoudel21@gmail.com>
2021-12-21 21:16:53 -0500

    Most part of website completed.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-//importing the main layout for the page that has 4 elemets.
-//Right now only the sidebar component is ready
 import Main from '../layouts/Main';
-
-//have to import the main layout to facilitate the website
 
 const Index = () => (
   <Main
-    description={
-      'Praful Poudel personal website. Current student at Wentworth Institute of Technology. '
-    }
+    description={"Praful Poudel personal website"}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2 data-testid="heading"> </h2>
-          {/* //post a link to a about me site */}
-          <p>A responsible, fast and stylish website using react.js.</p>
+          <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
+          <p>
+            A simple and responsive website built with REACTJS.
+          </p>
         </div>
       </header>
+      <p> Thank you for your time to visit this website. You can read more <Link to="/about">about me</Link>,
+        or you can check out my {' '}
+        <Link to="/resume">resume</Link>, {' '}
+        <Link to="/projects">projects</Link>, {' '}
+        view <Link to="/stats">stat</Link>, {' '}
+        or <Link to="/contact">contact</Link> me.
+      </p>
+      <p> Source available <a href="https://github.com/poudelpatwit/Personal-Website">here</a>.</p>
     </article>
   </Main>
-
-  //some links to the website other pages
 );
 
 export default Index;
-
-//this part is kind of complete
